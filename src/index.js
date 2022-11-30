dadd90a79f1cf564c9b535b49d30a2f7b4cb6155
Pavel-Kn <ferrari.inside@gmail.com>
2022-11-30 12:16:27 +0300

    creat redux App

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom/client';
+import { initiateStore } from './store/store';
+import * as actions from './store/actions'
+
+const store = initiateStore();
+
+const App = () => {
+ const [state, setState] = useState(store.getState())
+
+ useEffect (() => {
+  store.subscribe(()=> {
+    setState(store.getState);
+  });
+ },[]);
+
+ const completeTask = (taskId) => {
+  store.dispatch(actions.taskCompleted(taskId))
+ }
+
+ const changeTitle = (taskId) => {
+  store.dispatch(actions.titleChanged(taskId))
+ }
+
+  return (
+    <>
+      <h1>App</h1>
+      <ul>
+        {state.map((el) => (
+        <li key={el.id}>
+          <p>{el.title}</p>
+          <p>{`Completed: ${el.completed}`}</p>
+          <button onClick={()=>completeTask(el.id)}>complete</button>
+          <button onClick={()=>changeTitle(el.id)}>Change title</button>
+          <hr></hr>
+        </li>
+        ))}
+      </ul>
+    </>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
